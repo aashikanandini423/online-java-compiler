c142f176784779d142df2580599d3416161ebcdc
aashikanandini423 <aashika.nandini423@gmail.com>
2022-11-29 00:20:04 -0800

    Login response object

---
+++ src/main/java/com/codelearner/response/LoginResponse.java
@@ -0,0 +1,32 @@
+package com.codelearner.response;
+
+public class LoginResponse {
+
+	private String status;
+	private String userId;
+	private String role;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
