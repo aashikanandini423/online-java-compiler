eefca183037d7ac7596311409e6b5e5672431d5b
aashikanandini423 <aashika.nandini423@gmail.com>
2022-11-29 00:21:07 -0800

    Compile response objects

---
+++ src/main/java/com/codelearner/response/CompileResponse.java
@@ -0,0 +1,26 @@
+package com.codelearner.response;
+
+
+import java.util.List;
+
+public class CompileResponse {
+
+	private List<String> compiledOutput;
+	private boolean flag;
+
+	public List<String> getCompiledOutput() {
+		return compiledOutput;
+	}
+
+	public void setCompiledOutput(List<String> compiledOutput) {
+		this.compiledOutput = compiledOutput;
+	}
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+}
---
+++ src/main/java/com/codelearner/response/ProblemCode.java
@@ -0,0 +1,80 @@
+package com.codelearner.response;
+
+import java.util.List;
+
+public class ProblemCode {
+
+	private String problemId;
+	private String problemDesc;
+	private String problemTitle;
+	private List<String> codeLines;
+	private String answeredBy;
+	private String className;
+	private String feedback;
+	private String rating;
+
+	public String getProblemId() {
+		return problemId;
+	}
+
+	public void setProblemId(String problemId) {
+		this.problemId = problemId;
+	}
+
+	public String getProblemDesc() {
+		return problemDesc;
+	}
+
+	public void setProblemDesc(String problemDesc) {
+		this.problemDesc = problemDesc;
+	}
+
+	public String getProblemTitle() {
+		return problemTitle;
+	}
+
+	public void setProblemTitle(String problemTitle) {
+		this.problemTitle = problemTitle;
+	}
+
+	public List<String> getCodeLines() {
+		return codeLines;
+	}
+
+	public void setCodeLines(List<String> codeLines) {
+		this.codeLines = codeLines;
+	}
+
+	public String getAnsweredBy() {
+		return answeredBy;
+	}
+
+	public void setAnsweredBy(String answeredBy) {
+		this.answeredBy = answeredBy;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+}
