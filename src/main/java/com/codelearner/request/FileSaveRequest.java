b23adb8843c6f224ab7eeb1a0cb1acd24633140b
aashikanandini423 <aashika.nandini423@gmail.com>
2022-11-29 00:14:32 -0800

    Login user requests

---
+++ src/main/java/com/codelearner/request/FileSaveRequest.java
@@ -0,0 +1,81 @@
+package com.codelearner.request;
+
+
+import java.io.File;
+import java.util.List;
+
+public class FileSaveRequest {
+
+	private String questionId;
+	private File code;
+	private String problemDescription;
+	private String problemTitle;
+	private String language;
+	private String createdBy;
+	private List<String> participants;
+	private String answeredBy;
+
+	public String getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(String questionId) {
+		this.questionId = questionId;
+	}
+
+	public File getCode() {
+		return code;
+	}
+
+	public void setCode(File code) {
+		this.code = code;
+	}
+
+	public String getProblemDescription() {
+		return problemDescription;
+	}
+
+	public void setProblemDescription(String problemDescription) {
+		this.problemDescription = problemDescription;
+	}
+
+	public String getProblemTitle() {
+		return problemTitle;
+	}
+
+	public void setProblemTitle(String problemTitle) {
+		this.problemTitle = problemTitle;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public List<String> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(List<String> participants) {
+		this.participants = participants;
+	}
+
+	public String getAnsweredBy() {
+		return answeredBy;
+	}
+
+	public void setAnsweredBy(String answeredBy) {
+		this.answeredBy = answeredBy;
+	}
+}
