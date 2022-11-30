bff5eae94ed6b3878699a46a503cfccac4912881
aashikanandini423 <aashika.nandini423@gmail.com>
2022-11-30 11:47:57 -0800

    Interface for Mongo service

---
+++ src/main/java/com/codelearner/service/MongoService.java
@@ -0,0 +1,25 @@
+package com.codelearner.service;
+
+import com.codelearner.domain.Problem;
+import com.codelearner.request.FileSaveRequest;
+import com.codelearner.request.UpdateRequest;
+import com.codelearner.response.ProblemResponse;
+
+import java.util.List;
+
+public interface MongoService {
+
+	
+	List<String> fetchCode(String id, String criteria);
+
+	String saveCodeFile(FileSaveRequest fileSaveRequest, String userRole);
+	
+	Problem fetchFilesById(String id);
+	
+	List<ProblemResponse> fetchProblemsByLanguage(String language);
+
+	List<Problem> fetchAnswersforQuestion(String id);
+	
+	void updateFeedbackAndRating(UpdateRequest updateRequest);
+
+}
