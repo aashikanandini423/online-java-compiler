ba2c26aee269303c0bb9db27b9c1de595fe82a1f
aashikanandini423 <aashika.nandini423@gmail.com>
2022-11-30 11:43:00 -0800

    UserRepository

---
+++ src/main/java/com/codelearner/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.codelearner.repository;
+
+import com.codelearner.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUserIdAndPassword(String userId, String password);
+
+    List<User> findByRole(String role);
+
+}
